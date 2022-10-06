f84dd217b28a0af39d27cd0824f1d5568877abe1
Hai Nguyen Dang <76462148+zelvor@users.noreply.github.com>
2022-10-06 16:01:46 +0700

    tham khao code :)) dem ve sua

--- main.py
+++ main.py
@@ -1,112 +1,292 @@
-import pygame
+import pygame, sys, random
 
-BACKGROUND_COLOR = (150,255,150)
 
+class Paddle:
+	def __init__(self, screen, color, posX, posY, width, height):
+		self.screen = screen
+		self.color = color
+		self.posX = posX
+		self.posY = posY
+		self.width = width
+		self.height = height
+		self.state = 'stopped'
+		self.draw()
+
+	def draw(self):
+		pygame.draw.rect( self.screen, self.color, (self.posX, self.posY, self.width, self.height) )
+
+	def move(self):
+		if self.state == 'up':
+			self.posY -= 15
+		elif self.state == 'down':
+			self.posY += 15
+		elif self.state == 'left':
+			self.posX -= 15
+		elif self.state == 'right':
+			self.posX += 15
+
+	def clamp(self):
+		if self.posY <= 0:
+			self.posY = 0
+
+		if self.posY + self.height >= HEIGHT:
+			self.posY = HEIGHT - self.height
+
+	def restart_pos(self):
+		self.posY = HEIGHT//2 - self.height//2
+		self.state = 'stopped'
+		self.draw()
+
+class Ball:
+	def __init__(self, screen, color, posX, posY, radius):
+		self.screen = screen
+		self.color = color
+		self.posX = posX
+		self.posY = posY
+		self.dx = 0
+		self.dy = 0
+		self.radius = radius
+		self.draw()
+
+	def draw(self):
+		pygame.draw.circle( self.screen, self.color, (self.posX, self.posY), self.radius)
+
+	def start(self):
+		# this will be random
+		self.dx = 15
+		self.dy = 5
+
+	def move(self):
+		self.posX += self.dx
+		self.posY += self.dy
+
+	def wall_collision(self):
+		self.dy = -self.dy
+
+	def paddle_collision(self):
+		self.dx = -self.dx
+
+	def restart_pos(self):
+		self.posX = WIDTH//2
+		self.posY = HEIGHT//2
+		self.dx = 0
+		self.dy = 0
+		self.draw()
+
+class PlayerScore:
+	def __init__(self, screen, points, posX, posY):
+		self.screen = screen
+		self.points = points
+		self.posX = posX
+		self.posY = posY
+		self.font = pygame.font.SysFont("monospace", 80, bold=True)
+		self.label = self.font.render(self.points, 0, WHITE)
+		self.show()
+
+	def show(self):
+		self.screen.blit(self.label, (self.posX - self.label.get_rect().width // 2, self.posY))
+
+	def increase(self):
+		points = int(self.points) + 1
+		self.points = str(points)
+		self.label = self.font.render(self.points, 0, WHITE)
+
+	def restart(self):
+		self.points = '0'
+		self.label = self.font.render(self.points, 0, WHITE)
+
+class CollisionManager:
+	def between_ball_and_paddle1(self, ball, paddle):
+		ballX = ball.posX
+		ballY = ball.posY
+		paddleX = paddle.posX
+		paddleY = paddle.posY
+
+		# y is in collision area?
+		if ballY + ball.radius > paddleY and ballY - ball.radius < paddleY + paddle.height:
+			# x is in collision area?
+			if ballX - ball.radius <= paddleX + paddle.width:
+				# collision
+				return True
+
+		# no collision
+		return False
+
+	def between_ball_and_paddle2(self, ball, paddle):
+		ballX = ball.posX
+		ballY = ball.posY
+		paddleX = paddle.posX
+		paddleY = paddle.posY
+
+		# y is in collision?
+		if ballY + ball.radius > paddleY and ballY - ball.radius < paddleY + paddle.height:
+			# x is in collision?
+			if ballX + ball.radius >= paddleX:
+				# collision
+				return True
+
+		# no collision
+		return False
+
+	def between_ball_and_walls(self, ball):
+		ballY = ball.posY
+
+		# top collision
+		if ballY - ball.radius <= 0:
+			return True
+
+		# bottom collision
+		if ballY + ball.radius >= HEIGHT:
+			return True
+
+		# no collision
+		return False
+
+	def between_ball_and_goal1(self, ball):
+		return ball.posX + ball.radius <= 0
+
+	def between_ball_and_goal2(self, ball):
+		return ball.posX - ball.radius >= WIDTH
+
+# ---------------------------------------------
+
+# ---------
+# CONSTANTS
+# ---------
+WIDTH, HEIGHT = 1280, 720
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# SCREEN
 pygame.init()
-screen = pygame.display.set_mode((1280  , 720))
-pygame.display.set_caption("My Game")
+screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption('PONG')
+
+# ---------
+# FUNCTIONS
+# ---------
+def draw_board():
+	screen.fill( BLACK )
+	pygame.draw.line( screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5 )
+
+def restart():
+	draw_board()
+	score1.restart()
+	score2.restart()
+	ball.restart_pos()
+	paddle1.restart_pos()
+	paddle2.restart_pos()
+
+draw_board()
+
+# -------
+# OBJECTS
+# -------
+paddle1 = Paddle( screen, WHITE, 15, HEIGHT//2 - 60, 20, 120 )
+paddle2 = Paddle( screen, WHITE, WIDTH - 20 - 15, HEIGHT//2 - 60, 20, 120 )
+ball = Ball( screen, WHITE, WIDTH//2, HEIGHT//2, 15 )
+collision = CollisionManager()
+score1 = PlayerScore( screen, '0', WIDTH//4, 15 )
+score2 = PlayerScore( screen, '0', WIDTH - WIDTH//4, 15 )
+
+# ---------
+# VARIABLES
+# ---------
+playing = False
 clock = pygame.time.Clock()
 
-#collision function for rect and circle
-def collision(rleft, rtop, rwidth, rheight, center_x, center_y, radius):
-    rright, rbottom = rleft + rwidth, rtop + rheight
-
-    cleft, ctop = center_x - radius, center_y - radius
-    cright, cbottom = center_x + radius, center_y + radius
-
-    if rright < cleft or rleft > cright or rbottom < ctop or rtop > cbottom:
-        return False
-    
-    for x in (rleft, rleft + rwidth):
-        for y in (rtop, rtop + rheight):
-            if (center_x - x)**2 + (center_y - y)**2 < radius**2:
-                return True
-
-    if rleft <= center_x <= rright and rtop <= center_y <= rbottom:
-        return True
-    
-    return False        
-
-class SoccerGround:
-    def __init__(self):
-        self.image = pygame.image.load("images/soccer_ground.png")
-        self.image = pygame.transform.scale(self.image, (1280, 720))
-        self.rect = self.image.get_rect()
-        self.rect.center = (640, 360)
-
-    def draw(self):
-        screen.blit(self.image, self.rect)
-    
-    #draw 4 lines of the soccer ground
-    def draw_lines(self):
-        pygame.draw.line(screen, (0,0,0), (60, 35), (1220, 35), 10)
-        pygame.draw.line(screen, (0,0,0), (60, 685), (1220, 685), 10)
-        pygame.draw.line(screen, (0,0,0), (60, 35), (60, 685), 10)
-        pygame.draw.line(screen, (0,0,0), (1220, 35), (1220, 685), 10)
-       
-#player is a black bar
-class Player:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.size = 20
-        self.color = (0,0,0)
-        self.speed = 10
-    
-    def draw(self):
-        #bar
-        pygame.draw.rect(screen, self.color, (self.x, self.y, 10, 100))
-
-    def move(self):
-        if self.x > 1220:
-            self.x = 1220
-        if self.x < 60:
-            self.x = 60
-        if self.y > 685:
-            self.y = 685
-        if self.y < 35:
-            self.y = 35
-        key = pygame.key.get_pressed()
-        if key[pygame.K_a]:
-            self.x -= self.speed
-        if key[pygame.K_d]:
-            self.x += self.speed
-        if key[pygame.K_w]:
-            self.y -= self.speed
-        if key[pygame.K_s]:
-            self.y += self.speed
-        
-class Ball:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.size = 10
-        self.color = (0,0,0)
-        self.speed = 10
-    
-    def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
-
-
-soccer_ground = SoccerGround()
-player1 = Player(400, 300)
-
-ball = Ball(640, 360)
-
-
+# --------
+# MAINLOOP
+# --------
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
 
-    screen.fill(BACKGROUND_COLOR)
-    soccer_ground.draw()
-    soccer_ground.draw_lines()
-    player1.draw()
-    player1.move()
-    ball.draw()
-    if collision(player1.x, player1.y, 10, 100, ball.x, ball.y, ball.size):
-        print("collision")
-    pygame.display.update()
-    clock.tick(60)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_p and not playing:
+				ball.start()
+				playing = True
+
+			if event.key == pygame.K_r and playing:
+				restart()
+				playing = False
+
+			if event.key == pygame.K_w:
+				paddle1.state = 'up'
+
+			if event.key == pygame.K_s:
+				paddle1.state = 'down'
+			
+			if event.key == pygame.K_a:
+				paddle1.state = 'left'
+
+			if event.key == pygame.K_d:
+				paddle1.state = 'right'
+
+			if event.key == pygame.K_UP:
+				paddle2.state = 'up'
+
+			if event.key == pygame.K_DOWN:
+				paddle2.state = 'down'
+
+		if event.type == pygame.KEYUP:
+			paddle1.state = 'stopped'
+			paddle2.state = 'stopped'
+
+	if playing:
+		draw_board()
+
+		# ball
+		ball.move()
+		ball.draw()
+
+		# paddle 1
+		paddle1.move()
+		paddle1.clamp()
+		paddle1.draw()
+
+		# paddle 2
+		paddle2.move()
+		paddle2.clamp()
+		paddle2.draw()
+
+		# wall collision
+		if collision.between_ball_and_walls(ball):
+			print('WALL COLLISION')
+			ball.wall_collision()
+
+		# paddle1 collision
+		if collision.between_ball_and_paddle1(ball, paddle1):
+			print('COLLISION WITH PADDLE 1')
+			ball.paddle_collision()
+
+		# paddle2 collision
+		if collision.between_ball_and_paddle2(ball, paddle2):
+			print('COLLISION WITH PADDLE 2')
+			ball.paddle_collision()
+
+		# GOAL OF PLAYER 1 !
+		if collision.between_ball_and_goal2(ball):
+			draw_board()
+			score1.increase()
+			ball.restart_pos()
+			paddle1.restart_pos()
+			paddle2.restart_pos()
+			playing = False
+
+		# GOAL OF PLAYER 2!
+		if collision.between_ball_and_goal1(ball):
+			draw_board()
+			score2.increase()
+			ball.restart_pos()
+			paddle1.restart_pos()
+			paddle2.restart_pos()
+			playing = False
+
+	score1.show()
+	score2.show()
+
+	clock.tick(60)
+	pygame.display.update()
